ffb5e7233aab6204cc2619042c5856dc275d4c19
A.Zelinsky <zelen23@gmail.com>
2019-03-07 22:20:13 +0300

    addPoster

---
+++ src/main/java/com/mycompany/helper/Poster.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.helper;
+
+import com.vk.api.sdk.client.actors.UserActor;
+
+/**
+ *
+ * @author adolf
+ */
+public class Poster extends Thread{
+
+    Vk_api vk_api=new Vk_api();
+    @Override
+    public void run() {
+        //дата время
+        //интервал
+        //цикл в коллиество строк на отправку
+     
+    // vk_api.setPost(new UserActor(1212,"wewe"),"mess",1551919860);
+
+//To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+    
+    
+    
+}
