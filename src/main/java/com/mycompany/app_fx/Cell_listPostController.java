0a5762b713caba3a41662b477719ccfe75053d4a
A.Zelinsky <zelen23@gmail.com>
2019-03-18 00:24:59 +0300

    addScrollListener

--- src/main/java/com/mycompany/app_fx/Cell_listPostController.java
+++ src/main/java/com/mycompany/app_fx/Cell_listPostController.java
@@ -38,39 +38,38 @@ import javafx.scene.web.WebView;
  *
  * @author adolf
  */
-public class Cell_listPostController
-        
-{   
+public class Cell_listPostController {
+
     @FXML
     TextArea text;
-    
+
     @FXML
     TextField provideText;
-   
+
     @FXML
     WebView postPicture;
-    
+
     @FXML
     HBox hBox;
-    
+
     @FXML
     Label l_likes;
     @FXML
     Label l_viewers;
     @FXML
     Label l_date;
-    
+
     @FXML
     CheckBox check_toPost;
-    
-    Helper help=new Helper();
-   
+
+    Helper help = new Helper();
+    double i = 0;
+    int j = 0;
+
     /**
      * Initializes the controller class.
      */
-    
-   
-    
+
     public Cell_listPostController() {
         loadFXML();
     }
@@ -79,58 +78,60 @@ public class Cell_listPostController
         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Cell_listPost.fxml"));
             loader.setController(this);
-         
+
             loader.load();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-    
-   
-    
-    public void setInfo(final ConstructorPost string)
-    {
+
+    public void setInfo(final ConstructorPost string) {
         text.setText(string.text);
         provideText.setText(string.provId.toString());
-        l_likes.setText("likes: "+string.postLikes);
-        l_viewers.setText("views: : "+string.postViews);
+        l_likes.setText("likes: " + string.postLikes);
+        l_viewers.setText("views: : " + string.postViews);
         l_date.setText(help.convertTime(string.postdate));
         check_toPost.setSelected(string.flag);
-        WebEngine webEngine = postPicture.getEngine();
+        final WebEngine webEngine = postPicture.getEngine();
         webEngine.load(string.listPhoto.get(0));
-        
-       /* 
-        postPicture.setOnScroll(new EventHandler<ScrollEvent>(){
+        final int size = string.listPhoto.size();
+
+        postPicture.setOnScroll(new EventHandler<ScrollEvent>() {
             @Override
-            public void handle(ScrollEvent event) {    
-                System.err.println(event.getDeltaY());          
+            public void handle(ScrollEvent event) {
+
+                if (i < i + event.getDeltaY()) {
+                    if (j > 0) {
+                        j--;
+                        webEngine.load(string.listPhoto.get(j));
+                    }
+
+                } else {
+                    if (j < size - 1) {
+                        j++;
+                        webEngine.load(string.listPhoto.get(j));
+                    }
+                }
+
+                i = i + event.getDeltaY();
             }
         });
-        */
-        
-        check_toPost.selectedProperty().addListener(new ChangeListener<Boolean>(){
-                @Override
-                public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+
+        check_toPost.selectedProperty().addListener(new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
                 string.setSelected(newValue);
 
                 string.setText(text.getText());
                 /* тут же вносить именения в текст
                         (поправить сравняшку в постграббере и внести изменения в конструктор)*/
 
-                }
-            });
+            }
+        });
     }
-    
-    public HBox getBox()
-    {
+
+    public HBox getBox() {
         return hBox;
     }
 
-   
-
-   
-  
-
-    
 }
