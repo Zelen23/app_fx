d61f0d178c540ffc8ee8b871bc16b594e5c9527d
A.Zelinsky <zelen23@gmail.com>
2019-03-21 23:57:49 +0300

    addSelectorTimeToMainForm

--- src/main/java/com/mycompany/app_fx/FXMLController.java
+++ src/main/java/com/mycompany/app_fx/FXMLController.java
@@ -3,16 +3,23 @@ package com.mycompany.app_fx;
 import com.mycompany.helper.ConstructorPost;
 import com.mycompany.helper.ConstructorProvider;
 import com.mycompany.helper.DbHandler;
+import com.mycompany.helper.Helper;
 import com.mycompany.helper.PostGrabber;
 import com.mycompany.helper.Poster;
+import com.mycompany.helper.Vk_preferences;
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -22,15 +29,22 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 import javafx.util.Pair;
+
+
+/*! вывод в лог в файл
+если нет токена выкидывать окно
+*/
 
 public class FXMLController implements Initializable {
 
@@ -54,6 +68,17 @@ public class FXMLController implements Initializable {
     private Button button;
     @FXML
     private Button button2;
+    
+    @FXML
+    private TextField eH;
+    @FXML
+    private TextField eM;
+    
+    @FXML
+    private TextField eTimeInterval;
+    
+    @FXML
+    DatePicker datePick;
 
     @FXML
     private void handle_itemAuth(ActionEvent event) {
@@ -148,8 +173,10 @@ public class FXMLController implements Initializable {
                //System.err.println(elt.listPhoto);
             }
         }
-        Poster poster = new Poster(list);
+        Poster poster = new Poster(list,getDateTimeinEdit());
         poster.start();
+       
+        
        
     }
 
@@ -164,14 +191,7 @@ public class FXMLController implements Initializable {
             }
        
         }
-       /* 
-        providerList.add(529989036);
-        providerList.add(411014340);
-        providerList.add(408902013);
-        providerList.add(344417917);
-        providerList.add(419021587);
-        providerList.add(474456246);
-        */
+     
         postGrabber = new PostGrabber(providerList, postListView);
         postGrabber.start();
 
@@ -193,7 +213,8 @@ public class FXMLController implements Initializable {
         если есть то  в prividerList и auth-выставляем данные по пользователю из параметров
         все запросы к vkApi делать через выбраного пользоватея
         */
-
+       
+        setDateTime();
         List<String> aaa = new ArrayList<String>();
         aaa.add("s");
         List<ConstructorPost> constructorPosts = new ArrayList<>();
@@ -224,6 +245,44 @@ public class FXMLController implements Initializable {
          });
         
          */
+    }
+    
+    public void setDateTime(){
+        
+         Helper nowDate=new Helper();
+         String date[]=nowDate.convertTime(nowDate.unixTime()).split(" ");
+         String hours=date[1].split(":")[0];
+         String min=date[1].split(":")[1];
+      
+        datePick.setValue(LocalDate.parse(date[0], DateTimeFormatter.ISO_LOCAL_DATE));
+        eH.setText(hours);
+        eM.setText(min);
+   
+        Vk_preferences pref = new Vk_preferences();
+        final int vk_id=Integer.valueOf(pref.getPref(Vk_preferences.VK_USER_ID));
+        final DbHandler db=new DbHandler();
+        eTimeInterval.setText(""+db.settingsList("TimeInterval",vk_id));
+        eTimeInterval.textProperty().addListener(new ChangeListener<String>(){
+             @Override
+             public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                 db.insertSettings("TimeInterval",eTimeInterval.getText(),vk_id);   
+             }
+         });
+    }
+    
+    public Long getDateTimeinEdit(){
+       // "yyyy-MM-dd HH:mm"
+       LocalDate localDate=datePick.getValue();
+       String hh=eH.getText();
+       String mm=eM.getText();
+       String strDateTime= localDate+" "+hh+":"+mm;
+       
+       
+        
+      
+       System.err.println("local d "+strDateTime);
+    
+    return  new Helper().convertStrTimeToLong(strDateTime);
     }
 
     public class CheckedListViewCheckObserver<T> extends SimpleObjectProperty<Pair<T, Boolean>> {
